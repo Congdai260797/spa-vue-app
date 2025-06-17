8473e64ff76efe82a0e67690999259965f350f66
DAI Do Cong (VTI.D1) <dai.docong@vti.com.vn>
2025-06-17 11:01:16 +0700

    udapte

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -10,35 +10,33 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-import Loading from "../../shared/components/Loading.vue";
-import BookingForm from "./BookingForm.vue";
-import Articles from "./Articles.vue";
-import Feedback from "./Feedback.vue";
-import Banner from "./Banner.vue";
-import ClinicListing from "./ClinicListing.vue";
+  import { onMounted, ref } from 'vue';
+  import Loading from '../../shared/components/Loading.vue';
+  import BookingForm from './BookingForm.vue';
+  import Articles from './Articles.vue';
+  import Feedback from './Feedback.vue';
+  import Banner from './Banner.vue';
+  import ClinicListing from './ClinicListing.vue';
 
-import HomeService from "./home.service";
+  import HomeService from './home.service';
 
-const homeService = new HomeService();
-const isLoading = ref(false);
+  const homeService = new HomeService();
+  const isLoading = ref(false);
 
-const clinics = ref([]);
+  const clinics = ref([]);
 
-onMounted(async () => {
-  isLoading.value = true;
-  const response = await homeService.getListClinic();
-
-  if (response) {
-    const getClinics = response.data.clinics
-      .slice()
-      .sort((a, b) => b.rating - a.rating)
-      .slice(0, 4);
-
-    clinics.value = response.data.clinics.filter((item) =>
-      getClinics.includes(item)
-    );
-  }
-  isLoading.value = false;
-});
+  onMounted(async () => {
+    // isLoading.value = true;
+    // const response = await homeService.getListClinic();
+    // if (response) {
+    //   const getClinics = response.data.clinics
+    //     .slice()
+    //     .sort((a, b) => b.rating - a.rating)
+    //     .slice(0, 4);
+    //   clinics.value = response.data.clinics.filter((item) =>
+    //     getClinics.includes(item)
+    //   );
+    // }
+    // isLoading.value = false;
+  });
 </script>
